a0f3b7df4323b7e010f90f3e163560c83e512663
apache0505 <jiayulutsk@ukr.net>
2022-09-09 12:55:20 +0300

    hrt

---
+++ 02_JavaScript_full/04_loops/task_02/scripts.js
@@ -0,0 +1,10 @@
+const m = 5;
+const n = 20;
+let result = 1;
+for (let i = m; i <= n; i += 1) {
+  if (i % 2 === 0) {
+    continue;
+  }
+  result *= i;
+}
+console.log('Result: ' + result);
--- old/index copy.js
+++ old/index copy.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /**
  * @param {object[]} users
@@ -10,7 +10,7 @@ function findUsersByName(users, name) {
   let result = [];
 
   for (let i in users) {
-    if (users[i]['name'] === name) {
+    if (users[i]["name"] === name) {
       result.push(users[i]);
     }
   }
@@ -26,7 +26,7 @@ function findUsersByString(users, str) {
   // put your code here
   let result = [];
   for (let i in users) {
-    if (users[i]['name'].includes(str)) {
+    if (users[i]["name"].includes(str)) {
       result.push(users[i]);
     }
   }
@@ -37,53 +37,53 @@ function findUsersByString(users, str) {
 const users = [
   {
     id: 101,
-    name: 'Denis',
-    city: 'Kyiv',
+    name: "Denis",
+    city: "Kyiv",
   },
   {
     id: 102,
-    name: 'Alexandr',
-    city: 'Lviv',
+    name: "Alexandr",
+    city: "Lviv",
   },
   {
     id: 103,
-    name: 'Nastya',
-    city: 'Kyiv',
+    name: "Nastya",
+    city: "Kyiv",
   },
   {
     id: 104,
-    name: 'Violetta',
-    city: 'Odesa',
+    name: "Violetta",
+    city: "Odesa",
   },
   {
     id: 105,
-    name: 'Mykola',
-    city: 'Lviv',
+    name: "Mykola",
+    city: "Lviv",
   },
   {
     id: 106,
-    name: 'Denis',
-    city: 'Lviv',
+    name: "Denis",
+    city: "Lviv",
   },
   {
     id: 107,
-    name: 'Andrey',
-    city: 'Odesa',
+    name: "Andrey",
+    city: "Odesa",
   },
   {
     id: 108,
-    name: 'Alexey',
-    city: 'Dnipro',
+    name: "Alexey",
+    city: "Dnipro",
   },
 ];
 
-console.log(findUsersByName(users, 'Denis')); // ===> [ { id: 101, name: 'Denis', city: 'Kyiv' }, { id: 106, name: 'Denis', city: 'Lviv' } ]
-console.log(findUsersByName(users, 'Andrey')); // ===> [ { id: 107, name: 'Andrey', city: 'Odesa' } ]
-console.log(findUsersByName(users, 'Anna')); // ===> [ ]
+console.log(findUsersByName(users, "Denis")); // ===> [ { id: 101, name: 'Denis', city: 'Kyiv' }, { id: 106, name: 'Denis', city: 'Lviv' } ]
+console.log(findUsersByName(users, "Andrey")); // ===> [ { id: 107, name: 'Andrey', city: 'Odesa' } ]
+console.log(findUsersByName(users, "Anna")); // ===> [ ]
 
-console.log(findUsersByString(users, 'Al')); // ===> [ { id: 102, name: 'Alexandr', city: 'Lviv' }, { id: 108, name: 'Alexey', city: 'Dnipro' } ]
-console.log(findUsersByString(users, 't')); // ===> [ { id: 103, name: 'Nastya', city: 'Kyiv' }, { id: 104, name: 'Violetta', city: 'Odesa' } ]
-console.log(findUsersByString(users, 'Vik')); // ===> [ ]
+console.log(findUsersByString(users, "Al")); // ===> [ { id: 102, name: 'Alexandr', city: 'Lviv' }, { id: 108, name: 'Alexey', city: 'Dnipro' } ]
+console.log(findUsersByString(users, "t")); // ===> [ { id: 103, name: 'Nastya', city: 'Kyiv' }, { id: 104, name: 'Violetta', city: 'Odesa' } ]
+console.log(findUsersByString(users, "Vik")); // ===> [ ]
 
-console.log(findUsersByString(users, 'm')); // ===> [ ]
+console.log(findUsersByString(users, "m")); // ===> [ ]
 // explanation: letter 'm' is not included in any user name, 'Mykola' includes 'M' not 'm', so it is not added to the result
